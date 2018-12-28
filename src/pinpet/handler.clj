e8d096c488e57d2b6f69526896ff71277fc7a36a
Newton <newton.beck@gmail.com>
2018-12-28 20:14:53 -0200

    Configures JSON middleware and reads login's body

--- src/pinpet/handler.clj
+++ src/pinpet/handler.clj
@@ -1,11 +1,18 @@
 (ns pinpet.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.json :refer [wrap-json-body]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (POST "/api/login" request
+    (let [email (get-in request [:body "email"])
+          password (get-in request [:body "password"])]
+      (str email password)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+    wrap-json-body
+    (wrap-defaults api-defaults)))
