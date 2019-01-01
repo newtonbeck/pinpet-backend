2f20b78ff052e5cb6dad341b36f6c45335f15289
Newton <newton.beck@gmail.com>
2019-01-01 18:05:48 -0200

    Fixes database query

--- src/pinpet/login/database.clj
+++ src/pinpet/login/database.clj
@@ -2,9 +2,7 @@
   (:require [pinpet.config :as config]
             [clojure.java.jdbc :as jdbc]))
 
-(def db [{:email "luke@rebels.org", :password "x-wing"}])
-
-(defn find-user-by-credentials [credentials]
-  (let [email (:email credentials)
-        password (:password credentials)]
-    (jdbc/query ["select * from users where email = ? and password = ?"] email password)))
+(defn find-user-by-credentials [{:keys [email password]}]
+  (-> (config/read)
+    :db
+    (jdbc/query ["select * from users where email = ? and password = ?" email password])))
