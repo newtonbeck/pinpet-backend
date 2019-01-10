59f59b0e59eb43324a6d7c0ab80663f3135822ff
Newton <newton.beck@gmail.com>
2019-01-10 06:59:48 -0200

    Creates pets on database

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE `users` (
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `password` VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE `pets` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  `user_id` INT,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+);
