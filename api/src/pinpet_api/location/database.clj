ca6e1357bf97e7ede0ca1e19ca8b832907176d35
Newton <newton.beck@gmail.com>
2019-01-30 09:24:46 -0200

    Gets only the first location

--- api/src/pinpet_api/location/database.clj
+++ api/src/pinpet_api/location/database.clj
@@ -5,4 +5,5 @@
 (defn find-location-of-pet [{:keys [id]}]
   (-> (read-config)
     :db
-    (jdbc/query ["select * from locations where pet_id = ? order by id desc limit 1" id])))
+    (jdbc/query ["select * from locations where pet_id = ? order by id desc limit 1" id])
+    first))
