961512b15150431bddff57b4e46a1ef0977337ba
Newton <newton.beck@gmail.com>
2019-03-02 15:40:03 -0300

    Removes MQTT infra and consumer because chinese hardware uses proprietary protocol, not MQTT

--- consumer/src/pinpet_consumer/location/database.clj
+++
@@ -1,8 +0,0 @@
-(ns pinpet-consumer.location.database
-  (:require [clojure.java.jdbc :as jdbc]
-            [pinpet-consumer.config :refer [read-config]]))
-
-(defn add-location! [location]
-  (-> (read-config)
-    :db
-    (jdbc/insert! :locations location)))
--- consumer/test/pinpet_consumer/location/conversion_test.clj
+++
@@ -1,9 +0,0 @@
-(ns pinpet-consumer.location.conversion-test
-  (:use midje.sweet)
-  (:require [pinpet-consumer.location.conversion :as conversion]))
-
-(def payload (.getBytes "{\"lat\": 1.23, \"lon\": 4.56, \"hei\": 7.89, \"pet\": 1 }"))
-
-(facts "payload->location"
-  (fact "json payload"
-    (conversion/payload->location payload) => {:latitude 1.23 :longitude 4.56 :height 7.89 :pet_id 1}))
