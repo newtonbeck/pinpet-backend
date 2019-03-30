7e98124b9095142baaa1d87e7478ffc4a3cbdca9
Newton <newton.beck@gmail.com>
2019-03-30 18:47:17 -0300

    Creates the function which creates the JWT token

--- api/src/api/security/jwt.clj
+++ api/src/api/security/jwt.clj
@@ -1,4 +1,6 @@
-(ns api.security.jwt)
+(ns api.security.jwt
+  (:import [com.auth0.jwt.algorithms Algorithm]
+           [com.auth0.jwt JWT]))
 
 (defn claims? [request]
   (let [claims (:claims request)]
@@ -8,3 +10,13 @@
   (let [id (:sub claims)
         name (:name claims)]
     {:id id :name name}))
+
+(defn create-token [secret user]
+  (let [id-as-number (:id user)
+        id-as-text (str id-as-number)
+        name (:name user)
+        algorithm (Algorithm/HMAC256 secret)]
+    (-> (JWT/create)
+      (.withSubject id-as-text)
+      (.withClaim "name" name)
+      (.sign algorithm))))
--- api/test/api/security/jwt_spec.clj
+++ api/test/api/security/jwt_spec.clj
@@ -11,3 +11,9 @@
 (facts "claims->user"
   (fact "returns user from claims"
     (jwt/claims->user {:sub 1 :name "Luke Skywalker"}) => {:id 1 :name "Luke Skywalker"}))
+
+(def secret "f2b51a05-1f2f-4e83-b99e-c5e93214198d")
+
+(facts "create-token"
+  (fact "creates a new JWT token from secret and user"
+    (jwt/create-token secret {:id 1 :name "Luke Skywalker"}) => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmFtZSI6Ikx1a2UgU2t5d2Fsa2VyIn0.kwj65geVETSz5wyH083bqwI74WHi-7KUzzDOHyBPjk4"))
