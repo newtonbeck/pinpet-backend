4a51013461557e6acc9e31e72df6469c41de068a
Newton <newton.beck@gmail.com>
2019-03-30 17:02:20 -0300

    Configures JWT middleware

--- api/src/api/components/http_handler.clj
+++ api/src/api/components/http_handler.clj
@@ -1,15 +1,17 @@
 (ns api.components.http-handler
   (:require [com.stuartsierra.component :as component]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.jwt :refer [wrap-jwt]]))
 
 (defn- wrap-app-component [f db]
   (fn [request]
     (f (assoc request :db db))))
 
-(defn- make-handler [routes db]
+(defn- make-handler [routes db jwt-secret]
   (-> routes
     (wrap-app-component db)
+    (wrap-jwt {:alg :HS256 :secret jwt-secret})
     wrap-json-body
     wrap-json-response
     (wrap-defaults api-defaults)))
@@ -20,7 +22,8 @@
   (start [this]
     (let [db-pool (:db db)
           routes (:http-routes http-routes)
-          handler (make-handler routes db-pool)]
+          jwt-secret (get-in config [:config :jwt-secret])
+          handler (make-handler routes db-pool jwt-secret)]
       (assoc this :http-handler handler)))
 
   (stop [this]
