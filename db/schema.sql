55d81c653bd306fc5733259fe737cdd8f2e41853
Newton <newton.beck@gmail.com>
2019-04-09 07:11:44 -0300

    Revert database db to use underscore instead of hyphen

--- db/schema.sql
+++ db/schema.sql
@@ -7,23 +7,23 @@ CREATE TABLE `users` (
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `password` VARCHAR(255) NOT NULL,
-  `created-at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `pets` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(255),
-  `user-id` INT,
-  `created-at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`user-id`) REFERENCES `users`(`id`)
+  `user_id` INT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
 
 CREATE TABLE `locations` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `pet-id` INT,
+  `pet_id` INT,
   `latitude` REAL,
   `longitude` REAL,
   `height` REAL,
-  `created-at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`pet-id`) REFERENCES `pets`(`id`)
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`pet_id`) REFERENCES `pets`(`id`)
 );
